b73db045680ca7468ac6c53cd6bdf821a9665fa1
David92p <dave.panetta92@gmail.com>
2022-11-03 11:45:35 +0100

    add configs wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
-// module.exports = {
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-// }
+module.exports = {
+    entry: {
+        index: './src/js/script.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        port: 5000,
+        open: true,
+        static: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development'
+}
 
 // const path = require('path')
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
