118fe0eb8bbb77c1cbbeb3948bc6ffbf917d8ad4
David92p <dave.panetta92@gmail.com>
2022-11-09 16:19:06 +0100

    fix js file

---
+++ src/js/document.js
@@ -0,0 +1,8 @@
+import './city.json'
+
+// const cityValid =  async function (){
+//     const data = await fetch('https://developers.teleport.org/assets/urban_areas.json')
+//     console.log(data);
+// }
+
+// cityValid()
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,108 @@
 import '../css/style.css'
+const citylist = require('./json/citylist.json')
+
+let cities = [];
+
+for (let city of Object.keys(citylist)){
+    cities.push(city)
+}
+cities.sort()
+console.log(cities);
+const citiesListElement = document.querySelector('.autocomplete-list')
+const countryInput = document.querySelector('#search')
+
+// function loadData(data, element){
+//     element.innerHTML = ""
+//     let innerElement = ""
+//     data.forEach((item) => {
+//         innerElement += `<li>${item}</li>`
+//     });
+//     element.innerHTML = innerElement
+// }
+
+// function filterData(data, searchText){
+//     return data.filter((el) => el.toLowerCase().includes(searchText.toLowerCase()))
+// }
 
 
-let variabileProva = 'valore della variabile di prova'
+// countryInput.addEventListener('input', function (e) {
+//     e.preventDefault()
+//     const filteredData = filterData(cities, countryInput.value)
+//     loadData(filteredData, citiesListElement)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let city = document.getElementById('search')
+// let btn = document.getElementById('btn')
+
+// // 
+// async function a(valore){
+//     let data = await fetch(`https://api.teleport.org/api/cities/?${valore}`)
+//     let citta = data.json()
+//     return citta
+    
+// }
+
+// btn.addEventListener('click', () => {
+//     let dato = city.value
+//     console.log(a(dato));
+// })
+
+// let link = 'https://developers.teleport.org/assets/urban_areas.json'
+
+// async function x(){
+//     let data = await fetch(link)
+//     return data
+// }
+
+// console.log(x());
+
+
+
+
+
+
+
+
+
