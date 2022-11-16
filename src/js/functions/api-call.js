54a087dc4ec693f4cc0df42b5ce84c2fff2cc7af
David92p <dave.panetta92@gmail.com>
2022-11-16 12:07:41 +0100

    add comments

--- src/js/functions/api-call.js
+++ src/js/functions/api-call.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 const _ = require('lodash')
 
-// link api 
-const apiLink = 'https://api.teleport.org/api/cities/?search='
+//link api
 const api = 'https://api.teleport.org/api/'
+// link api con valore input
+const apiLink = 'https://api.teleport.org/api/cities/?search='
 
+//chiamata Api tramite valore di input
 export const getData = (city) => {
     const request =  axios.get(apiLink+city)
     return request
 }
+
 
