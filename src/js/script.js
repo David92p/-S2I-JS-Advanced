7eade76c5e67131ba9e8081643219a1edb16ed41
David92p <dave.panetta92@gmail.com>
2022-11-16 12:08:56 +0100

    import all useful functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,9 @@
 import '../css/style.css'
 
-import { getData } from './functions/api-call'
-import { removeElements, displayNames } from './functions/document'
+import { inputCreateList , btnValueCLick } from './functions/document'
 
-const input = document.getElementById('input')
-
-
-input.addEventListener("keyup", (e) => {
-    getData(input.value)
-    .then(response => {
-        const data = response.data
-        const cityData = data._embedded["city:search-results"]
-        const listCity = []
-        cityData.forEach(element => {
-            listCity.push(element.matching_full_name)
-        });
-        return listCity.slice(0,6)
-    })
-    .then(listCity => {
-        removeElements()
-        for (let city of listCity){
-            if(city.toLowerCase().startsWith(input.value.toLowerCase()) && input.value != ""){
-                let listItem = document.createElement('li')
-                listItem.classList.add("list-items")
-                listItem.style.cursor ="pointer"
-                listItem.setAttribute("onclick", "displayNames('" +
-                city + "')");
-                let word = "<b>" + city.substr(0, input.value.length) + "</b>"
-                word += city.substr(input.value.length)
-                listItem.innerHTML = word
-                document.querySelector(".list").appendChild(listItem)
-            }
-        }
-    })
-})
+inputCreateList()
+btnValueCLick()
 
 
 
