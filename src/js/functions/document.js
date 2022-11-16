d87e83f4820d59a8723b70fa14ea29d537007f4c
David92p <dave.panetta92@gmail.com>
2022-11-16 12:09:45 +0100

    inserted document functions

--- src/js/functions/document.js
+++ src/js/functions/document.js
@@ -1,34 +1,73 @@
+import { getData } from "./api-call"
 
-export const removeElements = () =>{
-    let items = document.querySelectorAll('.list-items')
+// -------------------- elements document  -------------------- //
+const btn = document.querySelector('.btn');
+const input = document.getElementById('input');
+
+// -------------------- functions documents  -------------------- //
+
+export const inputCreateList =  () => {
+    input.addEventListener("keyup", () => {
+        // richiesta dati api tramite valore campo input
+        getData(input.value)
+        // gestione dati e inserimento nome citta all'interno di un []
+        .then(response => {
+            const data = response.data
+            const cityData = data._embedded["city:search-results"]
+            const listCity = []
+            cityData.forEach(element => {
+                listCity.push(element.matching_full_name)
+            });
+            return listCity.slice(0,5);
+        })
+        // gestione dati e inserimento valore tramite click
+        .then(listCity => {
+            // puliamo inzialemente la lista <ul> da possibili <li> creati in precedenza 
+            removeElements();
+            // cicliamo le possibili città della lista composta dalla funzione async getData
+            for (let city of listCity){
+                // condizione per la creazione dei vari <li>
+                if(input.value != ""){
+                    // creazione <li>
+                    let listItem = document.createElement('li');
+                    // inseriamo style ed eventi click all'interno del <li> creato 
+                    listItem.classList.add("list-items");
+                    listItem.style.cursor ="pointer";
+                    listItem.addEventListener("click", () => displayNames(city));
+                    // inserimento valore input dentro <li> elemento
+                    let word = "<b>" + city.substr(0, input.value.length) + "</b>";
+                    word += city.substr(input.value.length);
+                    listItem.innerHTML = word;
+                    document.querySelector(".list").appendChild(listItem);
+                }
+            }
+        })
+    });
+} 
+
+//funzione elimina <li> da container <ul>
+const removeElements = () => {
+    const items = document.querySelectorAll('.list-items');
     items.forEach(el => el.remove())
 }
 
-export const displayNames = (value) => {
+// funzione stampa valore su display input
+const displayNames = (value) => {
     input.value = value
     removeElements()
 }
 
-//export const filterData = (data, searchText) => data.filter((el) => el.toLowerCase().includes(searchText.toLowerCase()))
+// event listener btn e chiamata api per valore inserito
+export const btnValueCLick = () => {
+    btn.addEventListener('click', () => {
+        let items = document.querySelectorAll('.list-items')
+        if(items.length == 0 && input.value != ""){
+            getData(input.value).then(res => {
+                let data = res.data
+                console.log(data);
+            })
+        }
+    })
+}
 
-// export const loadData = (data, element) => {
-//     const select = document.createElement('select')
-//     select.classList.add('autocomplete-list')
-//     select.setAttribute('name', 'autocomplete-list')
-//     element.append(select)
-
-    //element.innerHTML = ""
-    //let innerElement = ""
-    //.forEach((item) => {
-        // let option = document.createElement('option');
-        // option
-        // innerElement += `<option>${item}</option>`
-        // innerElement.setAttribute('value', item)
-        // element.innerHTML = innerElement
-//         let option = document.createElement('option')
-//         option.setAttribute('value', item)
-//         option.textContent = item
-//         select.append(option)
-//     });
-// }
 
