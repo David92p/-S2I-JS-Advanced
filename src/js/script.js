3ef78b9d0df572edeeb4236a1f6dfaecaed19c12
David92p <dave.panetta92@gmail.com>
2022-11-16 16:44:57 +0100

    fix functions to event listener - add event kp

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,20 @@ import '../css/style.css'
 
 import { inputCreateList , btnValueCLick } from './functions/document'
 
-inputCreateList()
-btnValueCLick()
+const input = document.getElementById('input');
+const btn = document.querySelector('.btn');
+
+
+//inputCreateList()
+input.addEventListener("keyup", inputCreateList)
+
+//Listener input entered by the user
+btn.addEventListener('click', btnValueCLick)
+document.addEventListener('keypress', (e) => {
+    if (e.code == "Enter"){
+        btnValueCLick()
+    }
+})
 
 
 
