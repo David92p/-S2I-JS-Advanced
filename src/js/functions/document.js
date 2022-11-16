60dcd6d59e7bf53ba8283bcb4dea813c784077a7
David92p <dave.panetta92@gmail.com>
2022-11-16 16:44:06 +0100

    fix event to arr func

--- src/js/functions/document.js
+++ src/js/functions/document.js
@@ -6,43 +6,41 @@ const input = document.getElementById('input');
 
 // -------------------- functions documents  -------------------- //
 
-export const inputCreateList =  () => {
-    input.addEventListener("keyup", () => {
-        // richiesta dati api tramite valore campo input
-        getData(input.value)
-        // gestione dati e inserimento nome citta all'interno di un []
-        .then(response => {
-            const data = response.data
-            const cityData = data._embedded["city:search-results"]
-            const listCity = []
-            cityData.forEach(element => {
-                listCity.push(element.matching_full_name)
-            });
-            return listCity.slice(0,5);
-        })
-        // gestione dati e inserimento valore tramite click
-        .then(listCity => {
-            // puliamo inzialemente la lista <ul> da possibili <li> creati in precedenza 
-            removeElements();
-            // cicliamo le possibili città della lista composta dalla funzione async getData
-            for (let city of listCity){
-                // condizione per la creazione dei vari <li>
-                if(input.value != ""){
-                    // creazione <li>
-                    let listItem = document.createElement('li');
-                    // inseriamo style ed eventi click all'interno del <li> creato 
-                    listItem.classList.add("list-items");
-                    listItem.style.cursor ="pointer";
-                    listItem.addEventListener("click", () => displayNames(city));
-                    // inserimento valore input dentro <li> elemento
-                    let word = "<b>" + city.substr(0, input.value.length) + "</b>";
-                    word += city.substr(input.value.length);
-                    listItem.innerHTML = word;
-                    document.querySelector(".list").appendChild(listItem);
-                }
+export const inputCreateList = () => {
+    // richiesta dati api tramite valore campo input
+    getData(input.value)
+    // gestione dati e inserimento nome citta all'interno di un []
+    .then(response => {
+        const data = response.data
+        const cityData = data._embedded["city:search-results"]
+        const listCity = []
+        cityData.forEach(element => {
+            listCity.push(element.matching_full_name)
+        });
+        return listCity.slice(0,5);
+    })
+    // gestione dati e inserimento valore tramite click
+    .then(listCity => {
+        // puliamo inzialemente la lista <ul> da possibili <li> creati in precedenza 
+        removeElements();
+        // cicliamo le possibili città della lista composta dalla funzione async getData
+        for (let city of listCity){
+            // condizione per la creazione dei vari <li>
+            if(input.value != ""){
+                // creazione <li>
+                let listItem = document.createElement('li');
+                // inseriamo style ed eventi click all'interno del <li> creato 
+                listItem.classList.add("list-items");
+                listItem.style.cursor ="pointer";
+                listItem.addEventListener("click", () => displayNames(city));
+                // inserimento valore input dentro <li> elemento
+                let word = "<b>" + city.substr(0, input.value.length) + "</b>";
+                word += city.substr(input.value.length);
+                listItem.innerHTML = word;
+                document.querySelector(".list").appendChild(listItem);
             }
-        })
-    });
+        }
+    })
 } 
 
 //funzione elimina <li> da container <ul>
@@ -57,17 +55,26 @@ const displayNames = (value) => {
     removeElements()
 }
 
+
+// export const btnValueCLick = () => {
+//     btn.addEventListener('click', () => {
+//         let items = document.querySelectorAll('.list-items')
+//         if(items.length == 0 && input.value != ""){
+//             getData(input.value).then(res => {
+//                 let data = res.data
+//                 console.log(data);
+//             })
+//         }
+//     })
+// }
+
 // event listener btn e chiamata api per valore inserito
 export const btnValueCLick = () => {
-    btn.addEventListener('click', () => {
-        let items = document.querySelectorAll('.list-items')
-        if(items.length == 0 && input.value != ""){
-            getData(input.value).then(res => {
-                let data = res.data
-                console.log(data);
-            })
-        }
-    })
-}
-
-
+    let items = document.querySelectorAll('.list-items')
+    if(items.length == 0 && input.value != ""){
+        getData(input.value).then(res => {
+            let data = res.data
+            console.log(data);
+        })
+    }
+};
