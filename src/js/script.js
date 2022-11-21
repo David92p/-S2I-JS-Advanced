afc4122e9226051ef21ab061bcde1accd14b430f
David92p <dave.panetta92@gmail.com>
2022-11-21 17:02:46 +0100

    add import function

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,25 @@
 import '../css/style.css'
 
 import { inputCreateList , btnValueCLick } from './functions/document'
+import { getDataCountry } from './functions/api-call';
 
 const input = document.getElementById('input');
 const btn = document.querySelector('.btn');
 
 
 //inputCreateList()
-input.addEventListener("keyup", inputCreateList)
+input.addEventListener("keyup", inputCreateList);
 
 //Listener input entered by the user
-btn.addEventListener('click', btnValueCLick)
+btn.addEventListener('click', btnValueCLick);
 document.addEventListener('keypress', (e) => {
     if (e.code == "Enter"){
         btnValueCLick()
     }
-})
+});
+
+getDataCountry()
+
 
 
 
