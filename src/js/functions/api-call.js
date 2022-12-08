bd68dba4e14d163ee5cfccbeb6762dc6a617d13a
David92p <Dave.panetta92@gmail.com>
2022-12-08 15:09:55 +0100

    deleted previous test code

--- src/js/functions/api-call.js
+++ src/js/functions/api-call.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 // import Chart from 'chart.js/auto'; //lib utile per grafico di dati della
 const _ = require('lodash')
-
 import cities from '../../json/citylist.json'
 
+// const 
+export const countriesNames = []
 // cicliamo il Json per avere una lista di nome delle città
-const countriesNames = []
 Object.keys(cities).forEach(city => {
     countriesNames.push(city)
 });
@@ -14,13 +14,9 @@ Object.keys(cities).forEach(city => {
 const casualCity = countriesNames[Math.floor(Math.random() * 265)]
 
 // link api
-// const api = 'https://api.teleport.org/api/';
-
+const api = 'https://api.teleport.org/api/';
+// environment variables
 const API_KEY = process.env.API_KEY;
-
-// link api country
-//const apiLinkCountry = 'https://api.teleport.org/api/countries/';
-
 
 // chiamata Api tramite valore di input
 export const getDataCity = (city) => {
@@ -32,62 +28,6 @@ export const getDataCity = (city) => {
 export const getCasualCity = () => {
     const citiesName = getDataCity(casualCity);
     return citiesName
-    // citiesName.then(response => {
-    //     const request = axios.get(response.data._embedded["city:search-results"][0]._links['city:item'].href)
-    //     return request
-    // })
-    // gestione promise con risultato array
-    // citiesName.then(response => {
-    //     // request data api
-    //     const request = axios.get(response.data._embedded["city:search-results"][0]._links['city:item'].href)
-    //     request.then(response => {
-    //         // link api data city
-    //         const linkData = response.data._links["city:urban_area"].href
-    //         // request data api
-    //         const data = axios.get(linkData)
-    //         return data
-    //     })
-    // })
 };
 
 
-// data2.then(response => {
-            //     const dataCity = response.data
-            //     const requestPhoto = axios.get(dataCity._links["ua:images"].href)
-            //     const requestDescription = axios.get(dataCity._links["ua:scores"].href)
-            //     const dataRequest = [
-            //         dataCity.full_nam, 
-            //         requestPhoto,
-            //         requestDescription
-            //     ]
-            //     dataRequest.push(dataCity.full_name)
-            //     dataRequest.push
-            //     const requestPhoto = axios.get(dataCity._links["ua:images"].href)
-            //     const requestDescription = axios.get(dataCity._links["ua:scores"].href)
-            //     requestPhoto.then(response => {
-            //         dataRequest.push(response.data.photos[0].image.web)
-            //         //console.log(dataRequest);
-            //     })
-            //     requestDescription.then(response => {
-            //         dataRequest.push(response.data.summary)
-            //     })
-            //     console.log(dataRequest);;
-            // })
-            // data2.then(response => {
-            //     // array dati card homepage [nome città - foto - descrizione]
-            //     const dataRequest = []
-            //     const dataCity = response.data
-            //     const nameCity = dataCity.full_name
-            //     dataRequest.push(nameCity)
-            //     const requestPhoto = axios.get(dataCity._links["ua:images"].href)
-            //     requestPhoto.then(response => {
-            //         dataRequest.push(response.data.photos[0].image.web)
-            //         return dataCity
-            //     }).then(response => {
-            //         const requestDescription = axios.get(dataCity._links["ua:scores"].href)
-            //         requestDescription.then(response => {
-            //             dataRequest.push(response.data.summary)
-            //             console.log(dataRequest);
-            //         })
-            //     })
-            // })
