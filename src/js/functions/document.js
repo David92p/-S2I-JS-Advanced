56804d8a5bc5f5f0edbd366f5e49f007005cf659
David92p <Dave.panetta92@gmail.com>
2022-12-08 15:12:03 +0100

    added search city list, fix selectable city

--- src/js/functions/document.js
+++ src/js/functions/document.js
@@ -1,66 +1,81 @@
-import { getDataCity, getCasualCity} from "./api-call"
 import axios from 'axios';
+// import async function 
+import { getCasualCity, getDataCity } from "./api-call";
+// import const lista città
+import { countriesNames } from './api-call';
 
-import cities from '../../json/citylist.json'
-
-// cicliamo il Json per avere una lista di nome delle città
-export const countriesNames = []
-Object.keys(cities).forEach(city => {
-    countriesNames.push(city)
-});
+// La funzione estrapola una lista di città dalla lista "countriesNames" in base al valore inserito dall'utente
+const createListCities = (value) => {
+    // convertiamo il valore inserito in valore minuscolo con lettera iniziale maiuscola
+    const valueUser = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    // lista di città selezionabili 
+    const citiesSelected = []
+    // ciclo sulla lunghezza del valore inserito
+    for (let i=0; i<valueUser.length; i++){
+        countriesNames.forEach(city => {
+            // condizione sul carattere selezionato dall'indice && controllo se elemento è già presente
+            if (city[i] != valueUser[i] && citiesSelected.includes(city)){
+                // eliminiamo la city dalla lista selezionabile 
+                citiesSelected.splice(citiesSelected.indexOf(city), 1)
+            }
+            // condizione sul carattere selezionato dall'indice && controllo se elemento NON è già presente
+            else if (city[i] == valueUser[i] && !citiesSelected.includes(city)){
+                // inseriamo la city all'interno della lista selezionabile
+                citiesSelected.push(city)
+                let index = 0
+                // controllo se ogni carattere dell'elemento inserito rispetta i caratteri del valore utente
+                while (index < valueUser.length){
+                    // condizione in caso di carattere diverso
+                    if (city[index] != valueUser[index]){
+                        // eliminiamo la city dalla lista selezionabile - indiciziamo a 0 - interrompiamo il ciclo
+                        citiesSelected.splice(citiesSelected.indexOf(city), 1)
+                        index = 0
+                        break
+                    }
+                    // incremento indice in caso di carattere uguale
+                    index += 1
+                }     
+            }
+        })
+    }
+    return citiesSelected.slice(0,5)
+}
 
 
 // La funzione effettua una chiamata all'api tramite un valore inserito dall'utente
 // La funzione manipola il dom creando gli <li></li> necessari in base al numero dei dati ricevuti dalla api fino ad un massimo di 5 elementi
 export const inputCreateList = () => {
-    // richiesta dati api tramite valore campo input
-    getDataCity(input.value)
-    // gestione dati e inserimento nome citta all'interno di un []
-    .then(response => {
-        const data = response.data
-        const cityData = data._embedded["city:search-results"]
-        console.log(cityData);
-        // const listCity = []
-        // cityData.forEach(element => {
-        //     listCity.push(element.matching_full_name)
-        // });
-        // return listCity.slice(0,5); 
-    })
-    // gestione dati e inserimento valore tramite click
-    // .then(listCity => {
-    //     // puliamo inzialemente la lista <ul> da possibili <li> creati in precedenza 
-    //     removeElements();
-    //     // cicliamo le possibili città della lista composta dalla funzione async getData
-    //     for (let city of listCity){
-    //         // condizione per la creazione dei vari <li>
-    //         if(input.value != ""){
-    //             // creazione <li>
-    //             let listItem = document.createElement('li');
-    //             // inseriamo style ed eventi click all'interno del <li> creato 
-    //             listItem.classList.add("list-items");
-    //             listItem.style.cursor ="pointer";
-    //             listItem.addEventListener("click", () => displayNames(city));
-    //             // inserimento valore input dentro <li> elemento
-    //             let word = "<b>" + city.substr(0, input.value.length) + "</b>";
-    //             word += city.substr(input.value.length);
-    //             listItem.innerHTML = word;
-    //             document.querySelector(".list").appendChild(listItem);
-    //         }
-    //     }
-    // })
-} 
+    const listCity = createListCities(input.value)
+    removeElements()
+    for (let city of listCity){
+        // condizione per la creazione dei vari <li>
+        if(input.value != ""){
+        // creazione <li>
+        let listItem = document.createElement('li');
+        // inseriamo style ed eventi click all'interno del <li> creato 
+        listItem.classList.add("list-items");
+        listItem.style.cursor ="pointer";
+        listItem.addEventListener("click", () => displayNames(city));
+        // inserimento valore input dentro <li> elemento
+        let word = "<b>" + city.substr(0, input.value.length) + "</b>";
+        word += city.substr(input.value.length);
+        listItem.innerHTML = word;
+        document.querySelector(".list").appendChild(listItem);
+        }
+    }
+};
 
 //funzione elimina <li> da container <ul>
 const removeElements = () => {
     const items = document.querySelectorAll('.list-items');
     items.forEach(el => el.remove())
-}
+};
 
 // funzione stampa valore su display input
 const displayNames = (value) => {
     input.value = value
     removeElements()
-}
+};
 
 // event listener btn e chiamata api per valore inserito
 export const btnValueCLick = () => {
@@ -126,48 +141,3 @@ export const casualImgCity = () => {
     })
 }
 
-// La funzione effettua una chiamata Api tramite un valore inserito casualmente da Js 
-// Api restituisce i dati necessari al dom per la creazione di una card 
-// Dati inseriti nel seguente modo [titolo città - foto città - testo introduttivo città]
-// export const createCard = () => {
-//     getCasualCity()
-//     // gestione promise con risultato array
-//     .then(response => {
-//         const request = axios.get(response.data._embedded["city:search-results"][0]._links['city:item'].href)
-//         return request
-//     })
-//     .then(response => {
-//         // link api data city
-//         const linkData = response.data._links["city:urban_area"].href
-//         // request data api
-//         const data = axios.get(linkData)
-//         return data
-//     })
-//     .then(response => {
-//         // lista dati card 
-//         const dataRequest = []
-//         const dataCity = response.data
-//         // nome città
-//         dataRequest.push(dataCity.full_name)
-//         // richiesta dati img 
-//         const requestPhoto = axios.get(dataCity._links["ua:images"].href)
-//         requestPhoto.then(response => {
-//             // push img [] dati card 
-//             dataRequest.push(response.data.photos[0].image.web)
-//             // richeista dati desdescrizione 
-//             const requestDescription = axios.get(dataCity._links["ua:scores"].href)
-//             requestDescription.then(response => {
-//                 // push descrizione [] dati card 
-//                 dataRequest.push(response.data.summary)
-//                 return dataRequest
-//             }).then(dataRequest => {
-//                 const imgCard = document.querySelector('.container-research')
-//                 const titleCard = document.querySelector('h2')
-//                 const descriptionCard = document.querySelector('p')
-//                 imgCard.style.backgroundImage = `url(${dataRequest[1]})` 
-//                 // titleCard.textContent = dataRequest[0]
-//                 //descriptionCard.textContent = dataRequest[2]
-//             })  
-//         })
-//     })
-// };
--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,9 @@
 import '../css/style.css'
-import { getDataCity } from './functions/api-call';
-
-import cities from '../json/citylist.json'
 
 import { inputCreateList , btnValueCLick, casualImgCity} from './functions/document'
 
 const input = document.getElementById('input');
 const btn = document.querySelector('.btn');
-
 
 // inputCreateList()
 input.addEventListener("keyup", inputCreateList);
@@ -22,45 +18,5 @@ document.addEventListener('keypress', (e) => {
 
 // funzione di caricamento img casuale sfondo header
 casualImgCity()
-
-const countriesNames = []
-Object.values(cities).forEach(city => {
-    countriesNames.push(city)
-});
-
-const cityProva = ['arezzo', 'aringa', 'aereo', 'bidone', 'bidello', 'posata', 'posate', 'posate', 'posate']
-
-const cercaCitta = (value) => {
-    const valueUser = value.toLowerCase()
-    const citiesSelected = []
-    
-    for (let i=0; i<valueUser.length; i++){
-        cityProva.forEach(city => {
-            if (city[i] != valueUser[i] && citiesSelected.includes(city)){
-                
-            }
-            // if (city[i] == valueUser[i] && !citiesSelected.includes(city)){
-            //     console.log(city);
-            // }
-            // if (!citiesSelected.includes(city)){
-            //     if (valueUser[i] == city[i]){
-            //         console.log(city);
-            //         //citiesSelected.push(city)
-            //     }
-            // }
-        })
-    }
-    // countriesNames.forEach(city => {
-    //     if (value[i] == city[i]){
-    //         citiesSelected.push(city)
-    //     } 
-    // })
-    //console.log(citiesSelected);
-}
-
-//cercaCitta('p')
-
-cercaCitta('ar');
-
 
 
