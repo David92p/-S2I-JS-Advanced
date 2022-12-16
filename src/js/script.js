77a41c8df51076fc70ee80c3e1aab4f0b21e8bbb
David92p <dave.panetta92@gmail.com>
2022-12-16 17:00:05 +0100

    fix events

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,33 @@
+import { Chart } from 'chart.js';
 import '../css/style.css'
+import { dataCollection } from './functions/async';
 
-import { inputCreateList, casualImgCity, createCanva} from './functions/document'
+import { inputCreateList, casualImgCity, createRandomCanvaScore, createCanvaScoreByUser} from './functions/document'
 
 const input = document.getElementById('input');
 const btn = document.querySelector('.btn');
+const myCanva = document.querySelector("#myCanva").getContext('2d')
 
 // inputCreateList()
 input.addEventListener("keyup", inputCreateList);
 
-// Listener input da parte dell'utente
-btn.addEventListener('click', createCanva);
+// Listeners input da parte dell'utente
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    createCanvaScoreByUser(myCanva)
+});
 document.addEventListener('keypress', (e) => {
     if (e.code == "Enter"){
-        createCanva()
+        createCanvaScoreByUser()
     }
 });
+
+// Listeners pagina
+window.addEventListener('load', () => {
+    createRandomCanvaScore(myCanva)
+})
 
 // funzione di caricamento img casuale sfondo header
 casualImgCity()
 
-// createCanva()
+
