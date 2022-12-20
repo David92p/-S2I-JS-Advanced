aa40eef70ebbf31ea16c6e6e430a178e4824e3bc
David92p <Dave.panetta92@gmail.com>
2022-12-20 21:53:47 +0100

    add const btn

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,41 @@
 import '../css/style.css'
 
-import { inputCreateList, createRandomCanvaScore, createCanvaScoreByUser} from './functions/document'
+import { inputCreateList, createCanvaCity} from './functions/document'
 
 const input = document.getElementById('input');
-const btn = document.querySelector('.btn');
+const btnSearch = document.querySelector('.btn-search');
+const btnScores = document.querySelector('.btn-scores');
+const btnDetails = document.querySelector('.btn-details');
+const btnSalaries = document.querySelector('.btn-salaries')
 const myCanva = document.querySelector("#my-canva").getContext('2d')
 
 // inputCreateList()
 input.addEventListener("keyup", inputCreateList);
 
 // Listeners input da parte dell'utente
-btn.addEventListener('click', (e) => {
+btnSearch.addEventListener('click', (e) => {
     e.preventDefault()
-    createCanvaScoreByUser(myCanva)
+    createCanvaCity(input.value)
 });
-document.addEventListener('keypress', (e) => {
-    if (e.code == "Enter"){
-        createCanvaScoreByUser()
-    }
-});
+// document.addEventListener('keypress', (e) => {
+//     if (e.code == "Enter"){
+//         createCanvaScoreByUser()
+//     }
+// });
+btnScores.addEventListener('click', () => {
+    console.log('scores avviato');
+})
+btnDetails.addEventListener('click', () => {
+    console.log('details avviato');
+})
+btnSalaries.addEventListener('click', () => {
+    console.log('salaries avviato');
+})
 
 // Listeners pagina
-window.addEventListener('load', () => {
-    createRandomCanvaScore(myCanva)
-});
+// window.addEventListener('load', () => {
+//     createRandomCanvaScore(myCanva)
+// });
 
 
 
