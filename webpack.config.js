d0ee8eec73dc6be094d5de2f6ebb09dc831d7eeb
David92p <dave.panetta92@gmail.com>
2022-12-27 16:16:07 +0100

    added img module

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(jpe?g|png|webp)$/i,
+            type: 'asset/resource'
         }
     ]
     },
