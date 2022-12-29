95b62c044c7a04afd87aa5e1a4fa026193cdbea6
David92p <dave.panetta92@gmail.com>
2022-12-29 16:50:31 +0100

    fix style icon

--- src/img/icon.js
+++ src/img/icon.js
@@ -1,7 +1,7 @@
 export const createIcon =  (img) =>  {
     const logo = new Image()
     logo.src = img
-    logo.style.width = '25px'
-    logo.style.height = '25px'
+    logo.style.width = '20px'
+    logo.style.height = '20px'
     return logo
 }
