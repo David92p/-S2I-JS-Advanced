da0e70648533db8265804a9cd0e6e197731fc2b0
David92p <Dave.panetta92@gmail.com>
2023-01-02 20:16:19 +0100

    set key api in async fnc

--- src/js/functions/api-call.js
+++ src/js/functions/api-call.js
@@ -14,21 +14,11 @@ export const casualCity = countriesNames[Math.floor(Math.random() * 265)]
 
 // chiamata Api tramite valore di input
 export const getDataCity = async (city) => {
-    // environment variables
-    const API_KEY = process.env.API_KEY;
-    const request = await axios.get(API_KEY+city)
-    return request
+    const API_KEY = "https://api.teleport.org/api/cities/?search="
+    const response = await axios.get(`${API_KEY+city}`)
+    return response
 };
 
-// async function callLambdaFunction() {
-//     // qui la magia: facciamo una chiamata ad una funzione che creeremo fra poco in un file a parte e che Netlify chiama dal proprio back-end in modo sicuro e privato quando necessario
-//     const response = await fetch("/.netlify/functions/lambda");
-//     const data = await response.json();
-  
-//     console.log(response); // Facciamo ciò che vogliamo coi dati ottenuti
-//   }
-  
-// callLambdaFunction();
 
 
 
