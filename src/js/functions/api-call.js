039508beb0720123eaffbd32fe7c70ea7f34a115
David92p <dave.panetta92@gmail.com>
2023-01-02 15:20:27 +0100

    add api key in asyn fnc

--- src/js/functions/api-call.js
+++ src/js/functions/api-call.js
@@ -12,14 +12,23 @@ Object.keys(cities).forEach(city => {
 // città generata casualmente dalla lista countriesNames
 export const casualCity = countriesNames[Math.floor(Math.random() * 265)]
 
-// environment variables
-const API_KEY = process.env.API_KEY;
-
 // chiamata Api tramite valore di input
 export const getDataCity = async (city) => {
+    // environment variables
+    const API_KEY = process.env.API_KEY;
     const request = await axios.get(API_KEY+city)
     return request
 };
+
+// async function callLambdaFunction() {
+//     // qui la magia: facciamo una chiamata ad una funzione che creeremo fra poco in un file a parte e che Netlify chiama dal proprio back-end in modo sicuro e privato quando necessario
+//     const response = await fetch("/.netlify/functions/lambda");
+//     const data = await response.json();
+  
+//     console.log(response); // Facciamo ciò che vogliamo coi dati ottenuti
+//   }
+  
+// callLambdaFunction();
 
 
 
